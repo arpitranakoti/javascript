ba7b2ce32cb5f8f14ca4821f7103a11199474f68
Arpit_Ranakoti <119879528+arpitranakoti@users.noreply.github.com>
2024-02-08 15:04:35 +0000

    variable & datatype

---
+++ Learn_JavaScript/01_variable.js
@@ -0,0 +1,21 @@
+const accountId=14453
+let accountEmail="Arpit@google.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+
+// accountId=2  //not allowed
+
+accountEmail="ar@gmail.com"
+accountPassword="231233"
+accountCity="Rishikesh"
+console.log(accountId)
+
+/* 
+prefer not to use var
+because of issue in block scope and functional scope
+ */
+
+console.table([accountEmail,accountPassword,accountCity])
+
+
+
---
+++ Learn_JavaScript/02_datatypr.js
@@ -0,0 +1,28 @@
+"use strict" // treat all js code as newer version
+
+// alert("3+3") // only for browser
+
+console.log(3+3)
+
+console.log("Arpit")
+  
+
+let name="Arpit"
+let age=21
+let isLoggedIn=false
+
+
+// number =>2^53
+// bigint
+// string => ""
+// boolean => true/false
+//null=> standalone value
+// undifined => let state;
+// symbol=> unique
+
+
+// object
+
+
+console.log(typeof undefined); // undefine
+console.log(typeof null); // object
