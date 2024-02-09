50fd7cbb8c419e34883b1560eda61443e62b86a1
Arpit_Ranakoti <119879528+arpitranakoti@users.noreply.github.com>
2024-02-09 14:22:47 +0000

    converion of datatype

---
+++ Learn_JavaScript/03_conversion.js
@@ -0,0 +1,26 @@
+// let score = "33abc"
+
+// console.log(typeof(score));
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber))
+// console.log( valueInNumber)
+
+//                                                         // "33 " => 33 
+//                                                         // "33abc" => NaN
+//                                                         //  true => 1 ; false =>0;
+
+
+//                                                         // let isloggedIn=1  //true
+//                                                         // let isloggedIn="" //=>false
+//                                                         //  "arpit" => true
+
+// let booleanIsLoggedIn=Boolean(isloggedIn)
+// console.log(booleanIsLoggedIn)
+
+let someNumber =33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
