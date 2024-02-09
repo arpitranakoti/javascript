e88ff9169b9043c407475efc59dfed648df94f4d
Arpit_Ranakoti <119879528+arpitranakoti@users.noreply.github.com>
2024-02-09 14:39:24 +0000

    operator added

--- Learn_JavaScript/03_conversion.js
+++ Learn_JavaScript/03_conversion.js
@@ -7,20 +7,48 @@
 // console.log(typeof(valueInNumber))
 // console.log( valueInNumber)
 
-//                                                         // "33 " => 33 
-//                                                         // "33abc" => NaN
-//                                                         //  true => 1 ; false =>0;
+//            // "33 " => 33 
+//            // "33abc" => NaN
+//            //  true => 1 ; false =>0;
 
 
-//                                                         // let isloggedIn=1  //true
-//                                                         // let isloggedIn="" //=>false
-//                                                         //  "arpit" => true
-
+//             //let isloggedIn=1  //true
+//            // let isloggedIn="" //=>false
+//            //  "arpit" => true
 // let booleanIsLoggedIn=Boolean(isloggedIn)
 // console.log(booleanIsLoggedIn)
 
 let someNumber =33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+
+// *********************************** Operations **********************
+
+let value =3
+let negValue=-value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2); // power w^2
+// console.log(2**3); // power w^3
+// console.log(2/3); //div
+// console.log(2%3);//rem 
+
+let str1="hello"
+let str2=" Arpit"
+
+let str3=str1+str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+"2");
+console.log("1"+2 + 2 );
+console.log(1+2 + "2" ); 
+
