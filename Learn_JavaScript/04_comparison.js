a8e7f7a3eb9744c0eab513118929d6638515f2bc
Arpit_Ranakoti <119879528+arpitranakoti@users.noreply.github.com>
2024-02-09 14:50:13 +0000

    comparison

---
+++ Learn_JavaScript/04_comparison.js
@@ -0,0 +1,16 @@
+// console.log("2">1)
+// console.log("02">1)
+
+// console.log(null>0);  // comparison convert null to number ,treating it as 0 thats why null>0 ->false
+//                         // and null>=0 ->true 
+// console.log(null==0);
+// console.log(null>=0);
+
+// console.log(undefined==0);
+// console.log(undefined >0);
+// console.log(undefined < 0);
+
+// === ( check both value and datatype)
+
+console.log("2"==2);
+console.log("2"===2);
