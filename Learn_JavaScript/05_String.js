9994009c156383e61842615d68ab67a334efbefc
Arpit_Ranakoti <119879528+arpitranakoti@users.noreply.github.com>
2024-02-11 14:37:08 +0000

    string_functions

---
+++ Learn_JavaScript/05_String.js
@@ -0,0 +1,32 @@
+let name = "Arpit"
+let count = 60
+console.log(name + count + " value");
+
+console.log(`hello my name is ${name} and my count is ${count} `);
+
+
+// string is an OBJECT 
+const gameName=new String('arpit ranakoti')
+console.log(gameName[0])
+console.log(gameName.__proto__)
+
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('z')); 
+console.log(gameName.valueOf());
+console.log(gameName.fontcolor("red"));
+const newString=gameName.substring(0,5)
+console.log(newString);
+
+// --------triming -----------
+const userTrim= "    user    "
+console.log(userTrim);
+console.log(userTrim.trim());
+console.log(userTrim.italics());
+ 
+
+
+
+
