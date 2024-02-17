23a169495d0b2937d13ca2662911393dbf9b1574
Arpit_Ranakoti <119879528+arpitranakoti@users.noreply.github.com>
2024-02-17 13:12:19 +0000

    rest operator

--- Learn_JavaScript/03_basic/05_function.js
+++ Learn_JavaScript/03_basic/05_function.js
@@ -1,10 +1,10 @@
 //funciton(keyword) f_name(){scope..}
 function egname(){
-    console.log("A");
-    console.log("R");
-    console.log("P");
-    console.log("I");
-    console.log("T");
+    // console.log("A");
+    // console.log("R");
+    // console.log("P");
+    // console.log("I");
+    // console.log("T");
 }
 
 // egname()
@@ -28,12 +28,40 @@ function loginUserMessage(username="sam"){
     // if(username===undefined)
     if(!username)
     {
-        console.log("please enter a username");
+        // console.log("please enter a username");
         return 
     }
     return `${username} just logged in.`
 }
 // console.log(loginUserMessage("Arpit"));
-console.log(loginUserMessage()); // undifined
+// console.log(loginUserMessage()); // undifined
 
+
+
+
+function calculateCartPrice(...num1){ // rest operator take multiple value return in [ x,y,z]
+    return num1
+}
+
+console.log(calculateCartPrice(200,400,500))
+
+const user={
+    username:"Arpit",
+    price:199
+}
+function handleObject(anyObject){
+    console.log(`username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+// handleObject(user)
+handleObject({
+    username:"hmm",
+    price:299
+})
+
+const myNewArray=[200,400,100,600]
+function returnSecondValue(getArray){
+    return getArray[2]
+}
+// console.log(returnSecondValue(myNewArray))
+console.log(returnSecondValue([200,400,500,1000]))
 
