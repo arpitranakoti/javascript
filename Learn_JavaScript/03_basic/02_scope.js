e4318dce5530472cfb411e3baf874f62ca693562
Arpit_Ranakoti <119879528+arpitranakoti@users.noreply.github.com>
2024-02-18 05:18:37 +0000

    scope level

--- Learn_JavaScript/03_basic/02_scope.js
+++ Learn_JavaScript/03_basic/02_scope.js
@@ -3,10 +3,59 @@ let a=300
 
 if(true){
     let a=10
-    console.log("INNER " ,a);
+    // console.log("INNER " ,a);
 const b=20
 var c =30
 }
-console.log(a);//error out of scope
+// console.log(a);//error out of scope
 // console.log(b);//same
 // console.log(c);//work 
+
+function one(){
+    const username="Arpit"
+
+    function two(){
+        const website="yt"
+        // console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+one()
+
+if(true){
+    const username="ar"
+    if(username==="ar")
+    {
+        const website=" yt"
+        // console.log(username+website);
+    }
+    // console.log(name)
+}
+// console.log(username);
+
+
+//++++++++++++++++++++++++++interesting +++++++++
+
+
+console.log(add1(5))
+function add1(num){
+    return num+1
+}
+
+
+
+
+// add2(5)  // error 
+
+const add2=function(num){
+    return num+2
+}
+
+console.log(add2(5))  
+
+
+
+
+
+
