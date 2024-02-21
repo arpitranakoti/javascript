27a7f4f2524b7d2e7a96647c764d13af6c6f7995
Arpit_Ranakoti <119879528+arpitranakoti@users.noreply.github.com>
2024-02-21 14:21:47 +0000

    switch case

--- Learn_JavaScript/04_control_flow/switch.js
+++ Learn_JavaScript/04_control_flow/switch.js
@@ -6,3 +6,26 @@
 //     default:
 //         break;
 // }
+
+// const month=3
+const month="april"
+
+switch (month) {
+    
+    case 1:
+        console.log("january");
+        break;
+    case "feb":
+        console.log("february");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case")
+        break;
+}
