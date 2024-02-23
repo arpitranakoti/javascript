ae5c575c0b9a1b5dc91e52e6f429e19353120943
Arpit_Ranakoti <119879528+arpitranakoti@users.noreply.github.com>
2024-02-23 14:48:51 +0000

    while loop

---
+++ Learn_JavaScript/05_iteration/two.js
@@ -0,0 +1,20 @@
+
+// let index=0
+// while(index<=10){
+//     // console.log(`value of index is ${index}`);
+//     index=index+2;
+// }
+
+// let arr=["Apit","Akshat","aman"]
+// let i=0;
+// while(i<arr.length){
+//     // console.log(`value is ${arr[i]}`);
+//     arr=arr+i;
+// }
+
+let score=11;
+do{
+    console.log(`Score is ${score}`);
+    score=score+1;
+    
+}while(score<=10);
