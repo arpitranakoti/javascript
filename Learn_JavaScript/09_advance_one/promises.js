7415b1f334bf4ffa2251285125cb1a0b54958057
Arpit_Ranakoti <119879528+arpitranakoti@users.noreply.github.com>
2024-04-12 18:02:17 +0000

    promises

--- Learn_JavaScript/09_advance_one/promises.js
+++ Learn_JavaScript/09_advance_one/promises.js
@@ -73,4 +73,60 @@ promise is an object */
                 .catch((error_aaiga)=>{
                     console.log(error_aaiga+" ||||jab error= true krainge|||||")
                 })
-                .finally(()=>console.log("The promise is either resolved or rejected but //*finally will allways run*/"))
+                .finally(()=>console.log("The promise is either resolved or rejected but //*finally will allways run***"))
+
+/*********************************** 5th promise********* */
+
+                const promiseFive=new Promise(function(resolve,reject){
+
+                    setTimeout(() => {
+                        let error = true
+                        // let error = false
+                        if(!error ){
+                            resolve({username:"5th_vala_promise",password:"123"})
+                        }
+                        else
+                        {
+                            reject('ERROR: 5th wala promise went wrong')
+                        }
+                    }, 1000);
+                })
+//similar to .then 
+                async function consumePromiseFive(){
+                   try
+                   {
+                    const response=await promiseFive
+                    console.log(response)
+                   }
+                   catch(error){
+                    console.log(error)
+                   }
+                }
+                
+                consumePromiseFive();
+
+/********************** */
+                // async function getAllUser(){
+                //    try{
+                //     const response=await fetch('https://jsonplaceholder.typicode.com/users')
+                //     // console.log(response);
+
+                //     // response.json() //convert response(string) to json (to extract data)
+                //     const data= await response.json() // response.json take time to convert so put await 
+                //     console.log(data);
+                //    }
+                //    catch(error)
+                //    {
+                //     console.log("E: " ,error);
+                //    }
+                // }
+                // getAllUser()
+
+        // same thing using .then 
+
+        fetch('https://api.github.com/users/arpitranakoti')
+        .then((response)=>{
+            return response.json()
+        })
+        .then((data)=>{console.log(data)})
+        .catch((error)=>console.log(error))
