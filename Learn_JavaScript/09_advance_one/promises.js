55d22e8773f214ef3db8cf0aba8525175d02ac9f
Arpit_Ranakoti <119879528+arpitranakoti@users.noreply.github.com>
2024-04-13 15:07:43 +0000

    fetch

--- Learn_JavaScript/09_advance_one/promises.js
+++ Learn_JavaScript/09_advance_one/promises.js
@@ -131,6 +131,26 @@ promise is an object
 
         // same thing using .then 
 
+
+ /*******************important question *****/
+    /*
+    Q: promises sai request kari aur vaha sai error aagya (eg 404 ) , tho vo resolve mai milaig ya reject mai ?
+    Ans : allway response mai milaiga ku ki usai nahi pta ki error aaya ya result 
+        error-> tab aaiga ja request hi send nahi kr paiga browser
+        explain in fetch_process img
+    */
+
+
+/* fetch('url','anything : obj , fuction that need to be send to browser') */
+
+
+fetch('https://api.github.com/users/arpitranakoti')
+.then((response)=>{
+    return response.json()
+})
+.then((data)=>{console.log(data)})
+.catch((error)=>console.log(error))
+
         fetch('https://api.github.com/users/arpitranakoti')
         .then((response)=>{
             return response.json()
