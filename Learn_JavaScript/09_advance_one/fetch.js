25d72754ceca3f7fb926d480133f6228caaf7b35
Arpit_Ranakoti <119879528+arpitranakoti@users.noreply.github.com>
2024-04-13 15:10:40 +0000

    fetch

--- Learn_JavaScript/09_advance_one/fetch.js
+++ Learn_JavaScript/09_advance_one/fetch.js
@@ -3,7 +3,7 @@
 Q: promises sai request kari aur vaha sai error aagya (eg 404 ) , tho vo resolve mai milaig ya reject mai ?
 Ans : allway response mai milaiga ku ki usai nahi pta ki error aaya ya result 
       error-> tab aaiga ja request hi send nahi kr paiga browser
-*/
+*/ 
 
 
 /* fetch('url','anything : obj , fuction that need to be send to url') */
